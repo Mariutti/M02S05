d02e04cca84cf5ec934c849d5d48215a8071da70
Pedro Alencar <pedromariutti@gmail.com>
2023-06-02 20:23:51 -0300

    Resolução dos exercícios 08 e 09

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,11 @@ import excecoes.ExcecaoDominio;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.temporal.Temporal;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws ParseException, ExcecaoDominio {
         String quarto;
         Date entrada;
         Date saida;
@@ -18,34 +16,30 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-            System.out.println("Qual o quarto que você gostaria de reservar?");
-            quarto = sc.nextLine();
-            System.out.println("Qual a data de entrada?");
-            entrada = parser.parse(sc.nextLine());
-            System.out.println("Qual a data de saída?");
-            saida = parser.parse(sc.nextLine());
+        System.out.println("Qual o quarto que você gostaria de reservar?");
+        quarto = sc.nextLine();
+        System.out.println("Qual a data de entrada (dd/mm/yyyy)?");
+        entrada = parser.parse(sc.nextLine());
+        System.out.println("Qual a data de saída (dd/mm/yyyy)?");
+        saida = parser.parse(sc.nextLine());
+
+        try {
             Reserva reserva1 = new Reserva(quarto, entrada, saida);
-        try {entradaDeDatas(reserva1.getTotalDias());
-        System.out.println("número do quarto " + reserva1.getQuarto());
-        System.out.println(parser.format(reserva1.getDataEntrada()));
-        System.out.println(parser.format(reserva1.getDataSaida()));
-        System.out.println(reserva1.getTotalDias() + " dias");
+            System.out.println(reserva1.toString());
+
+            System.out.println("Entre com as datas para atualização da reserva:");
+            System.out.println("Qual a data de entrada (dd/mm/yyyy)?");
+            Date novaEntrada = parser.parse(sc.nextLine());
+            System.out.println("Qual a data de saída (dd/mm/yyyy)?");
+            Date novaSaida = parser.parse(sc.nextLine());
+
+            reserva1.alterarReserva(novaEntrada, novaSaida);
+            System.out.println(reserva1.toString());
+
         } catch (ExcecaoDominio e) {
             System.out.println(e);
             System.out.println(e.getMessage());
         }
-
-
     }
-
-    public static void entradaDeDatas(int num) throws ExcecaoDominio {
-        if (num < 0) {
-            throw new ExcecaoDominio("Data de saída precisa ser posterior à data de entrada");
-        } else if (num == 0) {
-            throw new ExcecaoDominio("Data de saída precisa não pode ser igual à data de entrada");
-
-        }
-    }
-
 }
 
--- src/entidades/Reserva.java
+++ src/entidades/Reserva.java
@@ -1,5 +1,8 @@
 package entidades;
 
+import excecoes.ExcecaoDominio;
+
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 
@@ -7,25 +10,50 @@ public class Reserva {
     private String quarto;
     private Date dataEntrada;
     private Date dataSaida;
+    SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reserva() {
     }
 
-    public Reserva(String quarto, Date dataEntrada) {
-        this.quarto = quarto;
-        this.dataEntrada = dataEntrada;
-    }
-    public Reserva(String quarto, Date dataEntrada, Date dataSaida) {
+    public Reserva(String quarto, Date dataEntrada, Date dataSaida) throws ExcecaoDominio {
         this.quarto = quarto;
         this.dataEntrada = dataEntrada;
         this.dataSaida = dataSaida;
+        this.checarDatas(this.dataEntrada, this.dataSaida);
     }
 
-    public int getTotalDias(){
-        Duration totTempo = Duration.between( dataEntrada.toInstant(), dataSaida.toInstant());
+    public int getTotalDias() {
+        Duration totTempo = Duration.between(dataEntrada.toInstant(), dataSaida.toInstant());
         return (int) totTempo.toDays();
     }
 
+    public int getTotalDias(Date data1, Date data2) {
+        Duration totTempo = Duration.between(data1.toInstant(), data2.toInstant());
+        return (int) totTempo.toDays();
+    }
+
+    public void checarDatas(Date data1, Date data2) throws ExcecaoDominio {
+        if (this.getTotalDias(data1, data2) < 0) {
+            throw new ExcecaoDominio("Erro de Reserva: Data de saída deve ser maior que data de Entrada.");
+        } else if (this.getTotalDias(data1, data2) == 0) {
+            throw new ExcecaoDominio("Erro de Reserva: Data de saída não pode ser igual à data de entrada");
+        }
+    }
+    public void checarDatas(Date data1, Date data2, String msg1, String msg2) throws ExcecaoDominio {
+        if (this.getTotalDias(data1, data2) < 0) {
+            throw new ExcecaoDominio(msg1);
+        } else if (this.getTotalDias(data1, data2) == 0) {
+            throw new ExcecaoDominio(msg2);
+        }
+    }
+    public void alterarReserva(Date novaEntrada, Date novaSaida) throws ExcecaoDominio {
+        checarDatas(novaEntrada, this.dataEntrada, "Erro de Reserva: Datas da Reserva devem ser atualizada para datas " +
+                "futuras", "Erro de Reserva: Data de saída não pode ser igual à data de entrada");
+        this.dataEntrada = novaEntrada;
+        this.dataSaida = novaSaida;
+        checarDatas(this.dataEntrada, this.dataSaida, "Erro de Reserva: Data de saída deve ser maior que data de Entrada.", "Erro de Reserva: Data de saída não pode ser igual à data de " +
+                "entrada");
+    }
     public String getQuarto() {
         return quarto;
     }
@@ -48,5 +76,11 @@ public class Reserva {
 
     public void setDataSaida(Date dataSaida) {
         this.dataSaida = dataSaida;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva: Quarto " + this.getQuarto() + ", Entrada: " + parser.format(this.getDataEntrada()) + ", " +
+                "Saída: " + parser.format(this.getDataSaida()) + ", total de " + this.getTotalDias() + " dias ";
     }
 }
